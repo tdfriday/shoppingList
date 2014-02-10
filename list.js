ab2fd3b7c681a7bb259f2eb1449c47051c963c39
tdfriday <tdfriday@gmail.com>
2014-02-09 19:46:56 -0500

    Updated JS and CSS

---
+++ list.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+   $('#submit').click(function(){
+      addItem();
+      return false;
+});
+
+
+function addItem(){
+    var newItem = $('#newItem').val().trim();
+     console.log(newItem);
+    if (newItem === ""){
+   alert("Please enter something");
+   }
+   else {
+   $('.theList').append('<li class="items">' + newItem + '</li>');
+    $('#newItem').val("");
+   }
+   return false;
+   }
+
+
+$('#clearAll').click(function(){
+      $('.theList').empty();
+      return false;
+});
+
+$('#removeComplete').click(function(){
+      $('.completed').remove();
+      return false;
+});
+
+$('.items').click(function() {
+  $(this).toggleClass('completed');
+});
+
+$('#newItem').keypress(function(event){
+    if(event.keyCode == 13) {
+      $("#submit").click();
+      return false;
+    }
+    });
+
+});
